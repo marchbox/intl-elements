9fa6ada75234c1908b4af58e0d23f2dc4b469681
Zacky Ma <zacky@marchbox.com>
2022-11-11 22:25:50 -0800

    test: add more tests

--- src/elements/displaynames/displaynames.spec.ts
+++ src/elements/displaynames/displaynames.spec.ts
@@ -1,7 +1,7 @@
 import DisplayNames from './displaynames';
 
 describe('DisplayNames', () => {
-  it('produces consistent language names as the Intl API', async () => {
+  it('updates result text when both props and attributes change', async () => {
     const el = document.createElement('intl-displaynames') as DisplayNames;
     el.locales = 'en';
     el.of = 'ja';
@@ -19,6 +19,33 @@ describe('DisplayNames', () => {
     el.of = 'de';
     setTimeout(() => {
       expect(el.textContent).toBe('German');
+    }, 0);
+  });
+
+  it('produces consistent result as the Intl API', async () => {
+    const el = document.createElement('intl-displaynames') as DisplayNames;
+    el.locales = 'en';
+    el.of = 'ja';
+    document.body.append(el);
+
+    setTimeout(() => {
+      const intlResult = new Intl.DisplayNames('en', {
+        type: 'language',
+      }).of('ja');
+
+      expect(el.textContent).toBe(intlResult);
+    }, 0);
+
+    el.of = 'zh-Hant';
+    el.languageDisplay = 'standard';
+    setTimeout(() => {
+      const intlResult = new Intl.DisplayNames('en', {
+        type: 'language',
+        // @ts-ignore
+        style: 'standard',
+      }).of('zh-Hant')
+
+      expect(el.textContent).toBe(intlResult);
     }, 0);
   });
 });
