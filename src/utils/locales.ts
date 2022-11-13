d9e9fbaae57705824680d9bd9b86e7409484e82e
Zacky Ma <zacky@marchbox.com>
2022-11-12 23:38:13 -0800

    chore: add fixme

--- src/utils/locales.ts
+++ src/utils/locales.ts
@@ -3,6 +3,7 @@ export function localesToLocaleList(
   localeMatcher: Intl.RelativeTimeFormatLocaleMatcher
 ): Intl.BCP47LanguageTag[] {
   try {
+    // FIXME: Make this generic instead of being hardcoded to Intl.DisplayNames.
     return Intl.DisplayNames
         .supportedLocalesOf(locales.split(' '), {localeMatcher});
   } catch {
