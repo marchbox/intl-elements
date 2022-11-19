4ebb61fed3917ef5c0ffc6dccf8f2eff1c8632ff
Zacky Ma <zacky@marchbox.com>
2022-11-19 15:29:39 -0800

    test: test when locales changed

--- src/elements/listformat/listformat.spec.ts
+++ src/elements/listformat/listformat.spec.ts
@@ -4,6 +4,13 @@ import {createTestPage} from '../../testing';
 import HTMLIntlListItemElement from '../listitem/listitem';
 import HTMLIntlListFormatElement from './listformat';
 
+function getTextContent(el: HTMLIntlListFormatElement) {
+  const clonedEl = el.cloneNode(true) as HTMLIntlListFormatElement;
+  clonedEl.querySelectorAll('intl-listitem')
+      .forEach((el: HTMLIntlListItemElement) => el.remove());
+  return clonedEl.textContent?.trim() || '';
+}
+
 describe('ListForm', () => {
   it('produces consistent result as the Intl API', async () => {
     const page = await createTestPage<HTMLIntlListFormatElement>({
@@ -18,14 +25,16 @@ describe('ListForm', () => {
       `,
     });
 
-    const clonedEl = page.element.cloneNode(true);
-    clonedEl.querySelectorAll('intl-listitem')
-        .forEach((el: HTMLIntlListItemElement) => el.remove());
-
     // @ts-ignore
-    const intlResult = new Intl.ListFormat('en').format([
+    let intlResult = new Intl.ListFormat('en').format([
       'foo', 'bar', 'baz', 'qux']);
+    expect(getTextContent(page.element)).toBe(intlResult);
 
-    expect(clonedEl.textContent.trim()).toBe(intlResult);
+    page.element.setAttribute('locales', 'zh');
+    await page.element.updateComplete;
+    // @ts-ignore
+    intlResult = new Intl.ListFormat('zh').format([
+      'foo', 'bar', 'baz', 'qux']);
+    expect(getTextContent(page.element)).toBe(intlResult);
   });
 });
