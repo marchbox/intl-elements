f738ef08f6108a13668ee566fa71ccb4ae4f6f5d
Zacky Ma <zacky@marchbox.com>
2022-12-03 13:56:03 -0800

    test: fix intl-relativetimeformat tests

--- src/elements/relativetimeformat/relativetimeformat.spec.ts
+++ src/elements/relativetimeformat/relativetimeformat.spec.ts
@@ -1,50 +1,39 @@
 import {createTestPage} from '../../testing';
 import HTMLIntlRelativeTimeFormatElement from './relativetimeformat';
+import HTMLIntlRelativeTimeFormatFormatElement from './relativetimeformat-format';
 
 describe('intl-relativetimeformat', () => {
   it('has `value` property the same as its text content', async () => {
-    const page = await createTestPage<HTMLIntlRelativeTimeFormatElement>({
-      element: 'intl-relativetimeformat',
+    await createTestPage({
+      elements: ['intl-relativetimeformat', 'intl-relativetimeformat-format'],
       html: `
-        <intl-relativetimeformat locales="en"
-            format-unit="year" format-value="10">
+        <intl-relativetimeformat locales="en">
+          <intl-relativetimeformat-format>
+            <data value="10"></data>
+            <data value="year"></data>
+          </intl-relativetimeformat-format>
         </intl-relativetimeformat>
       `,
     });
+    const el = document.querySelector('intl-relativetimeformat-format') as HTMLIntlRelativeTimeFormatFormatElement;
+    const shadow = el.shadowRoot! as ShadowRoot;
+    const span = shadow.querySelector('span') as HTMLSpanElement;
 
-    expect(page.element!.value).toBe(page.element!.textContent.trim());
+    expect(el!.value).toBe(span.textContent?.trim());
   });
 
   it('returns correct resolved options', async () => {
-    const page = await createTestPage<HTMLIntlRelativeTimeFormatElement>({
-      element: 'intl-relativetimeformat',
+    await createTestPage({
+      elements: ['intl-relativetimeformat'],
       html: `
-        <intl-relativetimeformat locales="de"
-            format-unit="year" format-value="10">
+        <intl-relativetimeformat locales="de">
         </intl-relativetimeformat>
       `,
     });
-    const el = page.element;
+    const el = document.querySelector('intl-relativetimeformat') as HTMLIntlRelativeTimeFormatElement;
 
     const intlResult = new Intl.RelativeTimeFormat('de').resolvedOptions();
 
     expect(el.resolvedOptions()).toEqual(intlResult);
-  });
-
-  it('returns correct formated parts', async () => {
-    const page = await createTestPage<HTMLIntlRelativeTimeFormatElement>({
-      element: 'intl-relativetimeformat',
-      html: `
-        <intl-relativetimeformat locales="en"
-            format-unit="year" format-value="10">
-        </intl-relativetimeformat>
-      `,
-    });
-    const el = page.element;
-
-    const intlResult =
-        new Intl.RelativeTimeFormat('en').formatToParts(10, 'year');
-
-    expect(el.formattedParts).toEqual(intlResult);
   });
 });
