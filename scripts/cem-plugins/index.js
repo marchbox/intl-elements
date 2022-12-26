dfa21689b1c124ed957dae049c4651fdccb9dad1
Zacky Ma <zacky@marchbox.com>
2022-12-25 21:43:47 -0800

    docs: fix optionProperty decorator handler

--- scripts/cem-plugins/index.js
+++ scripts/cem-plugins/index.js
@@ -1,11 +1,11 @@
 import customJsDocTags from './custom-jsdoc-tags.js';
 import fixInheritedFieldTypes from './fix-inherited-field-types.js';
-// import optionPropertyDecorator from './option-property-decorator.js';
+import optionPropertyDecorator from './option-property-decorator.js';
 import removeMembers from './remove-members.js';
 
 export default [
   customJsDocTags,
-  // optionPropertyDecorator,
+  optionPropertyDecorator,
   fixInheritedFieldTypes,
   removeMembers,
 ];
--- scripts/cem-plugins/option-property-decorator.js
+++ scripts/cem-plugins/option-property-decorator.js
@@ -3,43 +3,43 @@ export default {
   analyzePhase: ({ts, node, moduleDoc}) => {
     if (node.kind === ts.SyntaxKind.ClassDeclaration) {
       const className = node.name.getText();
-      const classDeclaration = moduleDoc.declarations.find(declaration =>
+      const classDeclaration = moduleDoc.declarations?.find(declaration =>
           declaration.name === className);
 
       node.members?.forEach(member => {
         const memberName = member.name.getText();
         const messageField = classDeclaration.members.find(member =>
             member.name === memberName);
+        const optionPropertyDecorator = member?.decorators?.find(decorator =>
+            decorator.expression?.expression?.getText() === 'optionProperty');
 
-        // Handle `@optionProperty()` decorator
-        member?.decorators?.forEach(decorator => {
-          const decoratorName = decorator.expression?.expression?.escapedText;
+        if (optionPropertyDecorator) {
+          const attributeName = memberName
+              .toLowerCase()
+              .replace(/^option/, 'option-');
 
-          if (decoratorName === 'optionProperty') {
-            const attributeName = memberName
-                .toLowerCase()
-                .replace(/^option/, 'option-');
+          messageField.attribute = attributeName;
 
-            messageField.attribute = attributeName;
-
-            if (!classDeclaration.attributes) {
-              classDeclaration.attributes = [];
-            }
-
-            // @see https://github.com/open-wc/custom-elements-manifest/blob/master/packages/analyzer/src/features/analyse-phase/creators/createAttribute.js
-            const attribute = {
-              fieldName: member.name,
-              ...member,
-            };
-            delete attribute.kind;
-            delete attribute.static;
-            delete attribute.privacy;
-            delete attribute.reflects;
-            if (!classDeclaration.attributes.find(attr => attr.name === attributeName)) {
-              classDeclaration.attributes.push(attribute);
-            }
+          if (!classDeclaration.attributes) {
+            classDeclaration.attributes = [];
           }
-        });
+
+          const attribute = {
+            ...messageField,
+            name: attributeName,
+            fieldName: messageField.name,
+          };
+          delete attribute.kind;
+          delete attribute.static;
+          delete attribute.privacy;
+          delete attribute.reflects;
+          delete attribute.attribute;
+
+          if (!classDeclaration.attributes.find(
+              attr => attr.name === attributeName)) {
+            classDeclaration.attributes.push(attribute);
+          }
+        }
       });
     }
   },
